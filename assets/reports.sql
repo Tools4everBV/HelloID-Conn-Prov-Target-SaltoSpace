9f567deffb1067bd34e9fec6103588ae886f710a
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2025-02-03 14:01:24 +0100

    translated comment to english

--- assets/reports.sql
+++ assets/reports.sql
@@ -1,19 +1,18 @@
-/****** Ophalen van alle groepslidmaatschappen in Salto Space. 
-		Groepen ManagedBySync = 0 worden niet door HelloID gemanaged.******/
-SELECT 
-	UG.ManagedByDBSync, 
-	U.name, 
-	U.FirstName, 
-	U.LastName, 
-	U.Dummy2 ,
+/****** Retrieves all group memberships in Salto Space. 
+ Groups with ManagedBySync = 0 are not managed by HelloID. ******/
+SELECT
+	UG.ManagedByDBSync,
+	U.name,
+	U.FirstName,
+	U.LastName,
+	U.Dummy2,
 	G.name,
 	G.type,
 	G.status,
 	G.Description
-FROM 
+FROM
 	tb_Users_Groups as UG
-INNER JOIN 
-	tb_Users AS U ON U.id_user = UG.id_user
-INNER JOIN 
-	tb_Groups AS G ON UG.id_group = G.id_group
-ORDER BY Dummy2
+	INNER JOIN tb_Users AS U ON U.id_user = UG.id_user
+	INNER JOIN tb_Groups AS G ON UG.id_group = G.id_group
+ORDER BY
+	Dummy2
